7fc9d0ecfffc304a9bf8feffc3daec81acb58af8
Chloe <chloe@Chloe-MacBook-Pro.local>
2020-04-17 09:42:10 +0200

    option checkout book created

--- src/com/twu/biblioteca/Option.java
+++ src/com/twu/biblioteca/Option.java
@@ -1,8 +1,11 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class Option {
 
     private String name;
+    private String chosenBook;
 
     public Option(String name){
         this.name = name;
@@ -20,6 +23,11 @@ public class Option {
             case "Quit":
                 System.out.println("See you soon :)");
                 BibliotecaApp.quitApplication = true;
+                return;
+            case "Checkout a book":
+                Scanner scanner = new Scanner(System.in);   
+                System.out.println("Select the book you want to checkout");
+                chosenBook = scanner.nextLine().trim();
                 return;
             default:
                 System.out.println("Please select a valid option");
--- test/com/twu/biblioteca/optionTest.java
+++ test/com/twu/biblioteca/optionTest.java
@@ -1,5 +1,7 @@
 package com.twu.biblioteca;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -13,11 +15,21 @@ import static org.hamcrest.CoreMatchers.*;
 public class optionTest {
     private Option option1;
     private Option option2;
+    private Option option3;
+
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
 
     @Before
     public void setUpTheOptions(){
         option1 = new Option("List of books");
         option2 = new Option("Quit");
+        option3 = new Option("Checkout a book");
     }
 
     @Test
@@ -30,5 +42,11 @@ public class optionTest {
         option2.applyOption();
         assertThat(BibliotecaApp.quitApplication, is(true));
     }
-    
+
+//    @Test
+//    public void testOptionCheckoutBook() {
+//        option3.applyOption();
+//        assertEquals("Select the book you want to checkout\n", testOut.toString());
+//    }
+
 }
